17aa2002d1c4f919a5269333abf75a61ea4c6678
martul <mariia.tuliakova@soprasteria.com>
2019-03-27 15:28:38 +0100

    https://skl-tp.atlassian.net/browse/NTP-787 - aggTjänst använder nu aggCore med hsaCache och standardbehörighet och trädklättring - Tester för ny funktionalitet

--- GetAggregatedCarePlans/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareplans/integrationtest/GetAggregatedCarePlansIntegrationTest.java
+++ GetAggregatedCarePlans/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/logistics/logistics/getaggregatedcareplans/integrationtest/GetAggregatedCarePlansIntegrationTest.java
@@ -41,7 +41,6 @@ import java.util.List;
 import javax.xml.ws.Holder;
 import javax.xml.ws.soap.SOAPFaultException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,16 +48,19 @@ import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
 
 import riv.clinicalprocess.logistics.logistics.getcareplansresponder.v2.GetCarePlansResponseType;
 import riv.clinicalprocess.logistics.logistics.v3.CarePlanType;
-import se.skltp.agp.cache.TakCacheBean;
 import se.skltp.aggregatingservices.riv.clinicalprocess.logistics.logistics.getaggregatedcareplans.GetAggregatedCarePlansMuleServer;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusRecordType;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 import se.skltp.agp.test.consumer.AbstractAggregateIntegrationTest;
-import se.skltp.agp.test.consumer.ExpectedTestData;
+import se.skltp.agp.test.consumer.TestData;
 import se.skltp.agp.test.producer.EngagemangsindexTestProducerLogger;
 import se.skltp.agp.test.producer.TestProducerLogger;
 
 public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateIntegrationTest {
+
+    public GetAggregatedCarePlansIntegrationTest() {
+        super(rb.getString("TAK_TJANSTEKONTRAKT"));
+    }
 
     private static final Logger log = LoggerFactory.getLogger(GetAggregatedCarePlansIntegrationTest.class);
 
@@ -83,12 +85,6 @@ public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateInte
                "teststub-non-default-services/tak-teststub-service.xml";
     }
 
-
-    @Before
-    public void loadTakCache() throws Exception {
-    	final TakCacheBean takCache = (TakCacheBean) muleContext.getRegistry().lookupObject("takCacheBean");
-    	takCache.updateCache();
-    }
 
     /**
      * Perform a test that is expected to return zero hits
@@ -135,7 +131,7 @@ public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateInte
     @Test
     public void test_ok_one_hit() {
 
-        List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_ONE_HIT, 2, new ExpectedTestData(TEST_BO_ID_ONE_HIT, TEST_LOGICAL_ADDRESS_1));
+        List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_ONE_HIT, 2, new TestData(TEST_BO_ID_ONE_HIT, TEST_LOGICAL_ADDRESS_1));
 
         assertProcessingStatusDataFromSource(statusList.get(0), TEST_LOGICAL_ADDRESS_1);
     }
@@ -148,9 +144,9 @@ public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateInte
 
         // Setup call and verify the response, expect one booking from source #1, two from source #2 and a timeout from source #3
         List<ProcessingStatusRecordType> statusList = doTest(TEST_RR_ID_MANY_HITS, 3,
-            new ExpectedTestData(TEST_BO_ID_MANY_HITS_1, TEST_LOGICAL_ADDRESS_1),
-            new ExpectedTestData(TEST_BO_ID_MANY_HITS_2, TEST_LOGICAL_ADDRESS_2),
-            new ExpectedTestData(TEST_BO_ID_MANY_HITS_3, TEST_LOGICAL_ADDRESS_2));
+            new TestData(TEST_BO_ID_MANY_HITS_1, TEST_LOGICAL_ADDRESS_1),
+            new TestData(TEST_BO_ID_MANY_HITS_2, TEST_LOGICAL_ADDRESS_2),
+            new TestData(TEST_BO_ID_MANY_HITS_3, TEST_LOGICAL_ADDRESS_2));
 
         // Verify the Processing Status, expect ok from source system #1 and #2 but a timeout from #3
         assertProcessingStatusDataFromSource(statusList.get(0), TEST_LOGICAL_ADDRESS_1);
@@ -178,7 +174,7 @@ public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateInte
      * @param testData
      * @return
      */
-    private List<ProcessingStatusRecordType> doTest(String registeredResidentId, int expectedProcessingStatusSize, ExpectedTestData... testData) {
+    private List<ProcessingStatusRecordType> doTest(String registeredResidentId, int expectedProcessingStatusSize, TestData... testData) {
         return doTest(registeredResidentId, SAMPLE_SENDER_ID, SAMPLE_ORIGINAL_CONSUMER_HSAID, SAMPLE_CORRELATION_ID, expectedProcessingStatusSize, testData);
     }
 
@@ -192,7 +188,7 @@ public class GetAggregatedCarePlansIntegrationTest extends AbstractAggregateInte
      * @param testData
      * @return
      */
-    private List<ProcessingStatusRecordType> doTest(String registeredResidentId, String senderId, String originalConsumerHsaId, String correlationId, int expectedProcessingStatusSize, ExpectedTestData... testData) {
+    private List<ProcessingStatusRecordType> doTest(String registeredResidentId, String senderId, String originalConsumerHsaId, String correlationId, int expectedProcessingStatusSize, TestData... testData) {
 
         // Setup and perform the call to the web service
         GetAggregatedCarePlansTestConsumer consumer = new GetAggregatedCarePlansTestConsumer(DEFAULT_SERVICE_ADDRESS, senderId ,originalConsumerHsaId, correlationId);
